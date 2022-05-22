33463bcded006ab8cb63d2b3acd0d7e9c52c3ae0
Ellyse <85855848+ellysecarter@users.noreply.github.com>
2022-05-22 09:54:15 -0700

    starter files and setting up controller

---
+++ models/Pizza.js
@@ -0,0 +1,22 @@
+const { schema, model, Schema } = require('mongoose')
+const Pizza = model('Pizza', PizzaSchema)
+
+const PizzaSchema = new Schema({
+    pizzaName: {
+        type: String
+    },
+    createdBy: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    size: {
+        type: String,
+        default: 'Large'
+    },
+    toppings: []
+})
+
+module.exports = Pizza
